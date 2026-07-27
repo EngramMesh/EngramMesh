3d98a6cd5e8aba6a93cbbee802e0dac9d08e473b
ice <190520601@qq.com>
2026-07-27 19:06:34 +0800

    fix: reject authorization error payloads
    Signed-off-by: ice <190520601@qq.com>

--- services/src/engrammesh/modules/memory/application/errors.py
+++ services/src/engrammesh/modules/memory/application/errors.py
@@ -6,3 +6,6 @@ from typing import final
 @final
 class EpisodeAuthorizationDenied(PermissionError):
     """Raised when an actor cannot record an episode."""
+
+    def __init__(self) -> None:
+        super().__init__()
